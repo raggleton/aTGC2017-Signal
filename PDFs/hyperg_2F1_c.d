6f500179287dc15bcd86187c08d4f86d23d9bca5
Christoph Renner <undeo@student.kit.edu>
2017-04-19 14:21:46 +0200

    initial upload

---
+++ PDFs/hyperg_2F1_c.d
@@ -0,0 +1,6 @@
+
+# DO NOT DELETE
+
+./hyperg_2F1_c.so: sf_hyperg.h
+./hyperg_2F1_c.so: /cvmfs/cms.cern.ch/slc6_amd64_gcc481/lcg/root/5.34.18-cms4/include/cintdictversion.h /cvmfs/cms.cern.ch/slc6_amd64_gcc481/lcg/root/5.34.18-cms4/include/RVersion.h
+hyperg_2F1_c__ROOTBUILDVERSION= 5.34/18
